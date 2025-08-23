d1cce5b8620834eaf5c0ab11ecb5a1a670dbeb0d
marcelaamaro <marcelaamaromel@gmail.com>
2025-08-22 21:49:30 -0300

    Comecando aprender JavaScript

---
+++ javascript/condicionais.js
@@ -0,0 +1,10 @@
+// Exercicio 1
+let idade = prompt ("Qual e a idade?");
+
+if (idade > 18) {
+
+    alert ("Parabens! Voce e maior de idade.")
+    
+} else {
+    alert ("Vai pra super Nani!")
+}
---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+// console.log("Hello world");
+
+// let nome = prompt (" Qual seu nome? ");
+// let idade = prompt (" Qual a sua idade? ");
+
+// alert("Meu nome e " + nome + " e minha idade e " + idade);
+
+// console.log(" Meu nome e " + nome); 
+
+// let teste = "Azul";
+// console.log("Teste: ", teste);
+// teste = "Laranja";
+// console.log("Teste: ", teste);
+
+let idade = prompt ("Qual e a idade?");
+
+if (idade > 18) {
+    alert ("Parabens! Voce e maior de idade.")
+} else {
+    alert ("Vai pra super Nani!")
+}
